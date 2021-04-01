15e81d5441a9e8c8cc9ecf76fffc6a87224fc2c4
Yolenta Panggabean <79127063+yolentap@users.noreply.github.com>
2021-04-01 23:10:30 +0700

    Menambah kode pemanggil css

--- edit.php
+++ edit.php
@@ -2,9 +2,11 @@
 <html>
 <head>
 	<title>Data Mahasiswa</title>
+	<link rel="stylesheet" href="styleindex.css">
 </head>
 <body>
- 
+ <div class="wrapper">
+  <div id="formContent">
 	<h2>Data Mahasiswa</h2>
 	<br/>
 	<a href="index.php">KEMBALI</a>
